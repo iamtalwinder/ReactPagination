a42d0f332104c7216d9b1c5a4859e9b25c0eb1bc
Talwinder Singh <singhtalwinder790@gmail.com>
2020-06-12 20:08:38 +0530

    basic scrolling

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,25 @@ import axios from "axios";
 import NumberGrid from "./components/numberGrid/NumberGrid";
 
 function App() {
-	// const [pages, setPages] = useState(null);
-	// const [loading, setLoading] = useState(true);
-	// const [error, setError] = useState(false);
+	const PER_PAGE = 10;
+	const [page, setPage] = useState(1);
+	const [numbers, setNumbers] = useState([]);
 
-	// useEffect(async () => {
-	// 	try {
-	// 		const result = await axios.get("/api/user/dataCount");
-	// 		setPages(Math.ceil(result.data.count / 10));
-	// 		setLoading(false);
-	// 	} catch (err) {
-	// 		console.log(err);
-	// 		setError(true);
-	// 	}
-	// }, []);
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const result = await axios.get(
+					`/api/user/getData/${(page - 1) * 10}/${PER_PAGE}`
+				);
+				setNumbers([...numbers, ...result.data]);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+		fetchData();
+	}, [page]);
 
-	// if (loading) return <p>Loading..</p>;
-	// if (error) return <p>Error</p>;
-	return <NumberGrid numbers={[1, 2, 3, 4, 5, 6, 7, 8, 9]} />;
+	return <NumberGrid numbers={numbers} page={page} setPage={setPage} />;
 }
 
 export default App;
--- client/src/components/numberGrid/NumberGrid.js
+++ client/src/components/numberGrid/NumberGrid.js
@@ -3,10 +3,18 @@ import NumberCard from "../numberCard/NumberCard";
 import "./NumberGrid.css";
 
 function NumberGrid(props) {
+	const handleScroll = (event) => {
+		if (
+			event.target.clientHeight + event.target.scrollTop >=
+			event.target.scrollHeight - 20
+		) {
+			props.setPage(props.page + 1);
+		}
+	};
 	return (
-		<div className="numberGridContainer">
-			{props.numbers.map((number) => (
-				<NumberCard number={number} />
+		<div className="numberGridContainer" onScroll={handleScroll}>
+			{props.numbers.map((numberObj) => (
+				<NumberCard key={numberObj.number} number={numberObj.number} />
 			))}
 		</div>
 	);
